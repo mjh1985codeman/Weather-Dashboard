957f42de0e270782cc351fcaf661fd90b4bd342a
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-07-20 18:59:06 -0500

    Coded first api call for the openweathermap api

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Here I am!!");
+
+// api call for current forecast
+fetch(
+  "https://api.openweathermap.org/data/2.5/weather?q=Nashville&appid=74711516f5fae5b0413ec705cb27b3dc"
+).then(function (response) {
+  console.log(response);
+  response.json().then(function (currentCityWeather) {
+    console.log(currentCityWeather);
+  });
+});
