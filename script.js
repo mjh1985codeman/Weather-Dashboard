c813ea572d26753abb86e63d9abf17b78f4e9075
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-07-21 19:02:27 -0500

    got first api call working

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 console.log("Here I am!!");
 
+// variables
+var apiKey = "74711516f5fae5b0413ec705cb27b3dc";
+var searchButton = document.getElementById("CitySearchButton");
+
+//$("#CitySearchButton").click {}
+var currentCitySearchInputText = document.getElementById("citySearchInputText");
+searchButton.addEventListener("click", handleSearchInput);
+
 // api call for current forecast
-fetch(
-  "https://api.openweathermap.org/data/2.5/weather?q=Nashville&appid=74711516f5fae5b0413ec705cb27b3dc"
-).then(function (response) {
-  console.log(response);
-  response.json().then(function (currentCityWeather) {
-    console.log(currentCityWeather);
-  });
-});
+
+// Api Variable to the current forecast for the city that is entered into the
+// search box.
+function searchCurrentCity(city) {
+  fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
+
+// function handleSearchInput
+
+function handleSearchInput(e) {
+  if (!currentCitySearchInputText.value) {
+    return;
+  }
+  e.preventDefault();
+  var searchedCity = currentCitySearchInputText.value.trim();
+  searchCurrentCity(searchedCity);
+  console.log(searchedCity);
+}
