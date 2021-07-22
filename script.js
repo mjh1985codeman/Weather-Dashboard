d4120c39500efa8e6f29ef4cb3906fbc63154b21
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-07-21 22:10:58 -0500

    added second api call to get and display the current city temp, wind and humidity

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var currentCitySearchInputText = document.getElementById("citySearchInputText");
 //Event Listner for Search Button
 searchButton.addEventListener("click", handleSearchInput);
 
-// Api Variable to the current forecast for the city that is entered into the
+// Api Variable to the current City, State, Lat and Lon for the city that is entered into the
 // search box.
 function searchCurrentCity(city) {
   fetch(
@@ -25,13 +25,37 @@ function searchCurrentCity(city) {
       console.log(data[0].state);
       document.getElementById("current-city-state").innerHTML +=
         " " + data[0].state;
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      // search for the api to get the current weather of the city that is searched.
+      fetch(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          //current Temp
+          console.log(data.main.temp);
+          document.getElementById("current-city-temp").innerHTML +=
+            " " + data.main.temp + " degrees.";
+          //current Wind
+          console.log(data.wind.speed);
+          document.getElementById("current-city-wind").innerHTML +=
+            " " + data.wind.speed + " mph.";
+          //current Humidity
+          console.log(data.main.humidity);
+          document.getElementById("current-city-humidity").innerHTML +=
+            " " + data.main.humidity;
+        });
     });
 }
 
-// Function that gets ran whent the search button is clicked.
+// Function that gets ran whent the search button is clicked to pass the city input into the api url.
 function handleSearchInput(e) {
   if (!currentCitySearchInputText.value) {
-    return;
+    return; //This right here is magic
   }
   e.preventDefault();
   var searchedCity = currentCitySearchInputText.value.trim();
